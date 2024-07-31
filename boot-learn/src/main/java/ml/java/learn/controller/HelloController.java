3cfb51afdda0f7ac9badc123f23d7715cc198679
lixiaoxiao08 <lixiaoxiao08@58.com>
2024-07-31 21:05:19 +0800

    引入lombok研发工具，快速编写getter setter 构造方法等

--- boot-learn/src/main/java/ml/java/learn/controller/HelloController.java
+++ boot-learn/src/main/java/ml/java/learn/controller/HelloController.java
@@ -1,11 +1,26 @@
 package ml.java.learn.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import ml.java.learn.bean.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Auther: jiml
  * @Date: 2024/7/31 20:52
  * @Description:
  */
 
-
+@RequestMapping(value = "helloController")
+@RestController
+@Slf4j
 public class HelloController {
+
+    @GetMapping(value = "greet")
+    public Result greet(@RequestParam() String name ) {
+        String greet = "hello " + name;
+        return Result.ok("hello ");
+    }
 }
