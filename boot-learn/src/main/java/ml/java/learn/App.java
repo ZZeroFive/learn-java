11a3443fe664fbb955145d45c2b94626e36d8266
lixiaoxiao08 <lixiaoxiao08@58.com>
2024-07-31 20:39:43 +0800

    初始化spring boot项目2

---
+++ boot-learn/src/main/java/ml/java/learn/App.java
@@ -0,0 +1,14 @@
+package ml.java.learn;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) {
+       SpringApplication.run(App.class, args);
+    }
+}
