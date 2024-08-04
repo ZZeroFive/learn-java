a7f00a71f865cafe265100005275063fe84a0e2a
lixiaoxiao08 <lixiaoxiao08@58.com>
2024-08-04 18:22:52 +0800

    rollback: remove devtools:need config 1. build project automatically 2.allow auto-make

--- boot-learn/src/main/java/ml/java/learn/controller/HelloController.java
+++ boot-learn/src/main/java/ml/java/learn/controller/HelloController.java
@@ -2,10 +2,7 @@ package ml.java.learn.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import ml.java.learn.bean.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Auther: jiml
@@ -20,9 +17,15 @@ public class HelloController {
 
     @GetMapping(value = "greet")
     public Result<String> greet( @RequestParam(required = true, name = "name") String name) {
-        String greet = "hello BaBy " + name;
+        String greet = "hello " + name;
         log.info("用户请求参数信息: {}", name);
         return Result.ok(greet);
+    }
+
+    @GetMapping(value = "/{id}/info")
+    public Result<Void> demoGetPathVariable(@PathVariable(value = "id") String id) {
+        log.info("用户请ID: {}", id);
+        return Result.ok();
     }
 
 
